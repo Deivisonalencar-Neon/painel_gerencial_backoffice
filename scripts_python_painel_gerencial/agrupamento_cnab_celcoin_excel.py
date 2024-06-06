536788e06b7319d64a29b964b81517fa91c401b7
Deivisonalencar-Neon <160673550+Deivisonalencar-Neon@users.noreply.github.com>
2024-06-06 20:27:33 -0300

    Create agrupamento_cnab_celcoin_excel.py
    script cnab celcoin excel

---
+++ scripts_python_painel_gerencial/agrupamento_cnab_celcoin_excel.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+pasta = r'G:\Drives compartilhados\arquitetura\Documentos\Arquitetura Operacional\BACKOFFICE FINANCEIRO\DASHBOARD\DASH_OPERACIONAL\LOOKER\Arquivos_diarios\BOLETOS_CELCOIN'
+
+dfs = []
+
+for arquivo in os.listdir(pasta):
+    if arquivo.endswith('.xlsx'):
+        caminho_arquivo = os.path.join(pasta,arquivo)
+        df_temp = pd.read_excel(caminho_arquivo)
+        dfs.append(df_temp)
+
+df = pd.concat(dfs, ignore_index=True)
